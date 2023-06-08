a803b3e5021e59f4ab928b220bd54ccd91fce4d8
Flynn <wphestiraid@gmail.com>
2023-06-08 22:09:17 +0900

    :recycle: 훅스 리팩토링

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -4,11 +4,13 @@ function useCounter(limit: number, callback?: () => void) {
   const [count, setCount] = useState(0);
 
   const increment = () => {
-    const nextCount = (count + 1) % limit;
-    setCount(nextCount);
-    if (nextCount === 0) {
-      callback?.();
-    }
+    setCount((prevCount) => {
+      const nextCount = (prevCount + 1) % limit;
+      if (nextCount === 0) {
+        callback?.();
+      }
+      return nextCount;
+    });
   };
 
   return {
