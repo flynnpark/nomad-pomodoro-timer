82332ebb09adb0fe8e9038fb1747d1315db014ad
Flynn <wphestiraid@gmail.com>
2023-06-08 18:12:02 +0900

    :lipstick: 파비콘 설정 추가 및 기본 에셋 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
+
+const faviconPlugin = process.env.NODE_ENV == 'production' ? vitePluginFaviconsInject() : false;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), faviconPlugin],
+});
